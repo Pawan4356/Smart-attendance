80de8e982e84a588df57ca55e4e3931d840ba46e
Pawan4356 <pawankumar27112005@gmail.com>
2025-06-20 18:38:24 +0530

    Using face-alignment library

--- face_recognition/face_alignment.py
+++ face_recognition/face_alignment.py
@@ -1,25 +1,24 @@
-import cv2
+import face_alignment
+from skimage import io
 import numpy as np
-import face_recognition
+import cv2
+
+fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False)
 
 def align_face(img):
-    face_locations = face_recognition.face_locations(img)
-    face_landmarks = face_recognition.face_landmarks(img)
+    preds = fa.get_landmarks(img)
+    if preds is None:
+        return img
 
-    if not face_landmarks:
-        return img  # return as-is if no landmarks
+    landmarks = preds[0]
+    left_eye = np.mean(landmarks[36:42], axis=0)  # left eye landmarks
+    right_eye = np.mean(landmarks[42:48], axis=0)
 
-    landmarks = face_landmarks[0]
-    left_eye = np.mean(landmarks["left_eye"], axis=0)
-    right_eye = np.mean(landmarks["right_eye"], axis=0)
-
-    # compute angle
-    dy = right_eye[1] - left_eye[1]
     dx = right_eye[0] - left_eye[0]
+    dy = right_eye[1] - left_eye[1]
     angle = np.degrees(np.arctan2(dy, dx))
-
-    # rotate image to align eyes horizontally
     eyes_center = tuple(((left_eye + right_eye) / 2).astype(int))
+
     rot_mat = cv2.getRotationMatrix2D(eyes_center, angle, 1.0)
     aligned = cv2.warpAffine(img, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
     return aligned
